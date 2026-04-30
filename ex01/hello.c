d135942a12bdb1ed47669474bc533361281e3a33
xiaotang1031 <18744291759@163.com>
2026-04-30 20:27:57 +0800

    第二次提交：增加scanf键盘输入功能

--- ex01/hello.c
+++ ex01/hello.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
 
 int main() {
-    printf("Hello, World!\n");
+    char name[100];
+    printf("请输入你的名字: ");
+    scanf("%99s", name);
+    printf("你好，%s！\n", name);
     return 0;
 }
